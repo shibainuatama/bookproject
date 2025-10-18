621a5734f96e4ee4dce6b92a9263425e1a5ab57b
matsuura <y-matsuura@grace.ocn.ne.jp>
2025-10-18 11:54:47 +0900

    third

--- book/management/commands/superuser.py
+++ book/management/commands/superuser.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.conf import settings
+import os
 
 User = get_user_model()
 
@@ -8,7 +9,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='matsuura').exists():
             User.objects.create_superuser(
-              username='matsuura',
+            #   username='matsuura',
+              username=os.environ.get('SUPERUSER_NAME'),
               email='',
-              password='arkray00'
+            #   password='arkray00'
+              password=os.environ.get('SUPERUSER_PASS')
             )
